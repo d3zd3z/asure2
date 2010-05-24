86374b0c4be392b7ff699360279fc8371bd74443
David Brown <asure@davidb.org>
2010-05-23 22:24:27 -0700

    Basic dir iterator.

--- asure.cc
+++ asure.cc
@@ -6,10 +6,12 @@
 #include <memory>
 #include <cstdio>
 #include <vector>
+#include <algorithm>
 
 using std::string;
 using std::printf;
 using std::vector;
+using std::cout;
 
 extern "C" {
 #include <sys/types.h>
@@ -20,15 +22,38 @@ extern "C" {
 }
 
 class dirnode {
-  typedef std::pair<string, struct stat *> node_t;
+  public:
+  struct node_t {
+    string name;
+    struct stat *stat;
+    node_t(string& a, struct stat *b) {
+      name = a;
+      stat = b;
+    }
+
+    bool operator<(const node_t& other) const {
+      return name < other.name;
+    }
+  };
   vector<node_t> dirs;
   vector<node_t> nondirs;
-  public:
-    dirnode(string path);
-    ~dirnode();
+  dirnode(string path);
+  ~dirnode();
 };
 
-dirnode::dirnode(string path)
+struct name_ino_t {
+  string name;
+  ino_t ino;
+  name_ino_t(string& n, ino_t& i) { name = n; ino = i; }
+  bool operator<(const name_ino_t& other) const {
+    return ino < other.ino;
+  }
+};
+
+// Read in all of the names in a given directory, pairing them up with their
+// inode number.
+static void
+get_names(string path, vector<name_ino_t>& result)
 {
   DIR *dirp = opendir(path.c_str());
   if (dirp == NULL)
@@ -45,37 +70,67 @@ dirnode::dirnode(string path)
     if (name == "." || name == "..")
       continue;
 
-    struct stat *sbuf = new struct stat;
-    int result = lstat((path + "/" + name).c_str(), sbuf);
-    if (result != 0)
-      throw 8;
-
-    const node_t &node = node_t(name, sbuf);
-    if (S_ISDIR(sbuf->st_mode))
-      dirs.push_back(node);
-    else
-      nondirs.push_back(node);
-    // std::auto_ptr<struct stat> buf (new struct stat);
-    // std::cout << buf << '\n';
-    // std::cout << name << '\n';
+    result.push_back(name_ino_t(name, ent->d_ino));
   }
 
-  // TODO: Make this cleanuppable.
-  int result = closedir(dirp);
-  if (result != 0)
+  // TODO: Do this cleanup even with exceptions.
+  int r = closedir(dirp);
+  if (r != 0)
     throw 7;
+}
+
+dirnode::dirnode(string path)
+{
+  vector<name_ino_t> names;
+  get_names(path, names);
+
+  // Stat all of the names, in inode order.
+  // std::sort(names.begin(), names.end(), ino_lt);
+  std::sort(names.begin(), names.end());
+  for (vector<name_ino_t>::iterator i = names.begin(); i != names.end(); i++) {
+    struct stat *sbuf = new struct stat;
+    int result = lstat((path + "/" + i->name).c_str(), sbuf);
+    if (result == 0) {
+      const node_t &node = node_t(i->name, sbuf);
+      if (S_ISDIR(sbuf->st_mode))
+        dirs.push_back(node);
+      else
+        nondirs.push_back(node);
+    } else {
+      // TODO: Warn about unable to stat.
+      delete sbuf;
+    }
+  }
+
+  // Sort the results by name.
+  std::sort(dirs.begin(), dirs.end());
+  std::sort(nondirs.begin(), nondirs.end());
 }
 
 dirnode::~dirnode()
 {
   typedef vector<node_t>::iterator iter;
   for (iter i = dirs.begin(); i != dirs.end(); i++)
-    delete i->second;
+    delete i->stat;
   for (iter i = nondirs.begin(); i != nondirs.end(); i++)
-    delete i->second;
+    delete i->stat;
+}
+
+void walk(string path, string name)
+{
+  dirnode here(path);
+  typedef vector<dirnode::node_t>::iterator iter;
+  cout << "Enter " << name << "\n";
+  for (iter i = here.dirs.begin(); i != here.dirs.end(); i++) {
+    walk(path + '/' + i->name, i->name);
+  }
+  for (iter i = here.nondirs.begin(); i != here.nondirs.end(); i++) {
+    cout << "Item  " << i->name << '\n';
+  }
+  cout << "Leave " << name << "\n";
 }
 
 int main()
 {
-  dirnode root(".");
+  walk(".", "__root__");
 }
