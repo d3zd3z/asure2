432531f67baa551be23d6276ff063523eccfbd71
David Brown <asure@davidb.org>
2010-05-29 21:22:09 -0700

    Clean up program invocation.
    Remove the testing code from the main file.  Check for a command line
    argument, and require it to be the only valid command.

--- asure.cc
+++ asure.cc
@@ -2,105 +2,43 @@
  */
 
 #include <iostream>
-#include <cstdio>
 #include <string>
-#include <vector>
-#include <tr1/memory>
 
 #include "dirstream.h"
-#include "dir.h"
-#include "hash.h"
 #include "surefile.h"
 
 using std::string;
-using std::vector;
 using std::cout;
-using std::tr1::shared_ptr;
 
-using asure::Hash;
-using asure::DirNodeProxy;
-using asure::Directory;
-
-using asure::stream::FsDirSource;
-using asure::stream::FsDirSourceProxy;
 using asure::stream::DirEntryProxy;
 
-static void indent(int level)
-{
-  for (int i = 0; i < level; i++)
-    cout << " ";
-}
+class usage_error : public std::exception {
+  public:
+    usage_error(string const& message) throw() : message_(message) { }
+    ~usage_error() throw() { }
+    const char* what() const throw() { return message_.c_str(); }
+  private:
+    const string message_;
+};
 
-void walk(string path, string name, int level)
-{
-  Directory here(path);
-  typedef vector<DirNodeProxy>::const_iterator iter;
-  indent(level);
-  cout << "Enter " << name << '\n';
-  {
-    const vector<DirNodeProxy>& dirs = here.getDirs();
-    for (iter i = dirs.begin(); i != dirs.end(); i++) {
-      walk(path + '/' + (*i)->name, (*i)->name, level + 1);
-    }
-  }
-  {
-    const vector<DirNodeProxy>& nondirs = here.getNonDirs();
-    for (iter i = nondirs.begin(); i != nondirs.end(); i++) {
-      indent(level+1);
-      cout << "Item " << (*i)->name << '\n';
-      if (S_ISREG((*i)->stat.st_mode)) {
-	Hash hash;
-	hash.ofFile(path + "/" + (*i)->name);
-	indent(level);
-	cout << "  hash: " << static_cast<string>(hash) << '\n';
-      }
-    }
-  }
-  indent(level);
-  cout << "Leave " << name << '\n';
-}
-
-void showAtts(const asure::stream::EntryProxy& entry)
-{
-  typedef asure::stream::Entry::Atts::const_iterator iter;
-  const asure::stream::Entry::Atts& atts = entry->getAtts();
-  const iter end = atts.end();
-  for (iter i = atts.begin(); i != end; ++i) {
-    printf("      %-5s: %s\n", i->first.c_str(), i->second.c_str());
-  }
-}
-
-void walk2(DirEntryProxy dirNode, int level)
-{
-  cout << "Enter: " << dirNode->getPath() << '/' << dirNode->getName() << "\n";
-  showAtts(dirNode);
-  FsDirSourceProxy here = dirNode->getDirSource();
-  typedef FsDirSource::DirIterator DI;
-  const DI diEnd = here->dirEnd();
-  for (DI i = here->dirBegin(); i != diEnd; ++i) {
-    cout << "  " << (*i)->getName() << '\t' << (*i)->getPath() << '\n';
-    walk2(*i, level + 1);
-  }
-
-  cout << "files:\n";
-  typedef FsDirSource::FileIterator FI;
-  const FI fiEnd = here->fileEnd();
-  for (FI i = here->fileBegin(); i != fiEnd; ++i) {
-    cout << "  " << (*i)->getName() << '\t' << (*i)->getPath() << '\n';
-    showAtts(*i);
-  }
-
-  cout << "leave:\n";
-}
-
-int main()
+int main(int argc, char const* const* argv)
 {
   try {
-    // walk(".", "__root__", 0);
-    string root = ".";
-    DirEntryProxy here = asure::stream::walkPath(root);
-    asure::saveSurefile("2sure", here);
-    // walk2(here, 0);
+    if (argc != 2)
+      throw usage_error("expecting a single argument");
+
+    string const command = argv[1];
+    if (command == "scan") {
+      string root = ".";
+      DirEntryProxy here = asure::stream::walkPath(root);
+      asure::saveSurefile("2sure", here);
+    } else
+      throw usage_error("unknown command");
+  }
+  catch (usage_error& err) {
+    cout << "Asure, version 2.00\n";
+    cout << "Usage: asure {scan}\n\n";
+    cout << err.what() << '\n';
   }
   catch (int ret) {
     cout << "Raised: " << ret << '\n';
