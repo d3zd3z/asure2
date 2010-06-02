723f0eb06769c1cb3f623d4e3a42c7af9def9c2c
David Brown <asure@davidb.org>
2010-06-02 11:12:19 -0700

    Build on nix and normal systems.
    Needs to refer to a couple of libraries to link boost on some systems.
    Set up enough stuff in the environment so that this will build on Nix
    as well.

--- nix-setup.sh
+++ nix-setup.sh
@@ -1,18 +1,52 @@
 #! /bin/sh
 
-# On Nix, boost is not managed by pkgconfig, so cmake is unable to
-# find it.  Query where the package is installed and use that.  This
-# makes the assumption that the package is installed as part of the
-# user profile, and not by the system, or even part of a collection.
-# This is not really ideal.
+# Nix doesn't seem to provide a nice way of finding things in the user
+# environment, so search ourselves.
 
-# Another option would be to try and track down the symlink for one of
-# the include files or libraries, and find the store base.
+search_path=(
+	~/.nix-profile
+	/var/run/current-system/sw
+	)
 
-BOOST=`nix-env -q --out-path --no-name boost`
+reduce()
+{
+	#echo "Reduce $1"
+	if [ -L "$1" ]; then
+		nix-store -q -R "$1" | tail -1
+		return
+	elif [ -f "$1" ]; then
+		reduce `dirname "$1"`
+		return
+	else
+		echo "Unknown entity $1"
+		exit 1
+	fi
+}
+
+# Scan and try to find a store path, for the symlink named $1.
+find_store()
+{
+	for p in "${search_path[@]}"; do
+		d="${p}/$1"
+		#echo "try $d"
+		if [ -a "$d" ]; then
+			reduce "$d"
+			return
+		fi
+	done
+	echo "Unable to find path for $1"
+	exit 1
+}
+
+BOOST=`find_store include/boost/version.hpp`
+
+# Find libpthread (and librt).
+libc=`find_store lib/libc.so`
+bzip=`find_store lib/libbz2.so`
 
 mkdir -p build
 cd build
+export CMAKE_PREFIX_PATH="${libc}:${bzip}"
 cmake \
 	-DBOOST_ROOT=${BOOST} \
 	-DCMAKE_BUILD_TYPE=DEBUG \
